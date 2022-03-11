18b0db1a06b89cce2fc1c9f17626c51d1284a16f
Vermeent <p.c.s.vermeent@uu.nl>
2022-03-11 09:12:16 +0100

    first bare working version of adapted Ospan

--- materials/ospan_adapted/0_functions.js
+++ materials/ospan_adapted/0_functions.js
@@ -1,18 +1,50 @@
-var test_display = function(selected_letters, current_letter){
+var cogloadf = function(){  // generates math questions
+  var possibleOperations = [" + ", " - "];
+  operation = jsPsych.randomization.sampleWithReplacement(possibleOperations, 1)[0];
+  if (operation==" + "){
+    num1 = Math.floor(jStat.uniform.sample(1, 11));
+    num2 =  Math.floor(jStat.uniform.sample(1, 11));
+    ans = num1 + num2;
+  } else if (operation==" - "){
+    num1 = Math.floor(jStat.uniform.sample(1, 11));
+    num2 = Math.floor(jStat.uniform.sample(1, num1));
+    ans = num1 - num2;
+  }
+  
+  var correct = jsPsych.randomization.sampleWithReplacement([true, false], 1)[0];
+  
+  if (!correct){   // generates incorrect answers
+    ansDiff = jsPsych.randomization.sampleWithReplacement([1,2],1)[0];
+    coinFlip = jsPsych.randomization.sampleWithReplacement([true, false],1)[0];
+    if (coinFlip){
+      ans += ansDiff;
+    } else {
+      ans -= ansDiff;
+    }
+    if (ans<0){
+      ans += 2*ansDiff; //ensuring no negative incorrect answers
+    }
+  }
+  
+  var equation = num1+operation+num2+' = '+ans+'<br><br><br><br>';
+  
+  return equation;
+};
+
+
+
+var test_display = function(selected_letters, current_letter, part){
     
-    console.log("current letter is:" + current_letter);
- 
-    var boxes = ["black", "black", "black", "black", "black", "black", "black"];
+    var boxes = ["black", "black", "black", "black", "black"];
     var letter1 = selected_letters[0];
     var letter2 = selected_letters[1];
     var letter3 = selected_letters[2];
+    var letter4 = selected_letters[3];
+    var letter5 = selected_letters[4];
     
-    var letter4 = (selected_letters.length > 3)? selected_letters[3] : "";
-    var letter5 = (selected_letters.length > 4)? selected_letters[4] : "";
-    var letter6 = (selected_letters.length > 5)? selected_letters[5] : "";
-    var letter7 = (selected_letters.length > 6)? selected_letters[6] : "";
+    var equation = (part === "cog_load")? cogloadf() : "";
     
-    // Determine which letteris uncovered during the letter encoding phase
+    // Determine which letter is reveiled during the letter encoding phase
     if(current_letter === 0) {
       boxes[0] = "white";
     }
@@ -28,22 +60,19 @@ var test_display = function(selected_letters, current_letter){
     if(current_letter == 4) {
       boxes[4] = "white";
     }
-    if(current_letter == 5) {
-      boxes[5] = "white";
-    }
-    if(current_letter == 6) {
-      boxes[6] = "white";
-    }
     
     html = "<style>";
     html += ".grid-container {";
     html += "display: grid;";
     html += "grid-template-columns: auto;";
-    html += "grid-template-rows: auto auto auto auto auto auto;";
+    html += "grid-template-rows: auto auto;";
     html += "padding: 0px;";
+    html += "position:relative;";
+    html += "justify-content: center;";
+    html += "text-align: center;";
     html += "}";
     html += ".grid-item {";
-    html += "padding: 0px;";
+   // html += "padding: 0px;";
     html += "justify-content: center;";
     html += "text-align: center;";
     html += "}";
@@ -51,43 +80,50 @@ var test_display = function(selected_letters, current_letter){
   
     html += "<div class='grid-container' style = 'height:400px';>";
     html += "<div class='grid-item'>";
-    html += "<div style = 'display: inline-grid; width: 800px; height: 90px; font-size: 70px; grid: 70px / auto auto auto auto auto auto auto;'>";
-    html += "<div style = 'margin: auto; border: solid black; background:" + boxes[0] + "; width: 90px; height: 90px; padding: 0px 0px;'>";
-    html += "<div style = 'top: 50%;left: 50%;width: 80px; height: 80px; border: solid red;'><span>" + letter1 + "</span></div>";
+    html += "<div style = 'display: inline-grid; width: 800px; height: 90px; font-size: 70px; grid: 70px / auto auto auto auto auto;'>";
+    html += "<div style = 'margin: auto; background:" + boxes[0] + "; width: 90px; height: 90px;'>";
+    html += "<div style = 'margin: auto; padding: 25px 0px'><span>" + letter1 + "</span></div>";
     html += "</div>";
-    html += "<div style = 'margin: auto; border: solid black; background:" + boxes[1] + "; width: 80px; height: 80px; padding: 0px 0px;'>";
-    html += "<div style = 'margin: auto; padding: 0px 0px; transform: rotate(0deg);'><span>" + letter2 + "</span></div>";
+    html += "<div style = 'margin: auto; background:" + boxes[1] + "; width: 90px; height: 90px;'>";
+    html += "<div style = 'margin: auto; padding: 25px 0px'><span>" + letter2 + "</span></div>";
     html += "</div>";
-    html += "<div style = 'margin: auto; border: solid black; background:" + boxes[2] + "; width: 80px; height: 80px; padding: 0px 0px;'>";
-    html += "<div style = 'margin: auto; padding: 0px 0px; transform: rotate(0deg);'><span>" + letter3 + "</span></div>";
+    html += "<div style = 'margin: auto; background:" + boxes[2] + "; width: 90px; height: 90px;'>";
+    html += "<div style = 'margin: auto; padding: 25px 0px'><span>" + letter3 + "</span></div>";
     html += "</div>";
-    html += "<div style = 'margin: auto; border: solid black; background:" + boxes[3] + "; width: 80px; height: 80px; padding: 0px 0px;'>";
-    html += "<div style = 'margin: auto; padding: 0px 0px; transform: rotate(0deg);'><span>" + letter4 + "</span></div>";
+    html += "<div style = 'margin: auto; background:" + boxes[3] + "; width: 90px; height: 90px;'>";
+    html += "<div style = 'margin: auto; padding: 25px 0px'><span>" + letter4 + "</span></div>";
     html += "</div>";
-    html += "<div style = 'margin: auto; border: solid black; background:" + boxes[4] + "; width: 80px; height: 80px; padding: 0px 0px;'>";
-    html += "<div style = 'margin: auto; padding: 0px 0px; transform: rotate(0deg);'><span>" + letter5 + "</span></div>";
-    html += "</div>";
-    html += "<div style = 'margin: auto;border: solid black; background:" + boxes[5] + ";  width: 80px; height: 80px; padding: 0px 0px;'>";
-    html += "<div style = 'margin: auto; padding: 0px 0px; transform: rotate(0deg);'><span>" + letter6 + "</span></div>";
-    html += "</div>";
-    html += "<div style = 'margin: auto;border: solid black; background:" + boxes[6] + ";  width: 80px; height: 80px; padding: 0px 0px;'>";
-    html += "<div style = 'margin: auto; padding: 0px 0px; transform: rotate(0deg);'><span>" + letter7 + "</span></div>";
+    html += "<div style = 'margin: auto; background:" + boxes[4] + "; width: 90px; height: 90px;'>";
+    html += "<div style = 'margin: auto; padding: 25px 0px'><span>" + letter5 + "</span></div>";
     html += "</div>";
     html += "</div></div>";
-    html += "<div class='grid-item' style = 'font-size: 70px; color: black'><br><br><br><br><br><br>1+1=2<br><br><br><br></div>";
+    html += "<div class='grid-item' style = 'font-size: 70px; color: black'><br><br><br><br><br><br>" + equation + "<br><br><br><br></div>";
   
     return html;
 };
 
 
-var load_display = function(){
+
+var attention_cue = function(next_letter){
+ 
+    var boxes = ["black", "black", "black", "black", "black"];
     
-    console.log("selected_letters = " + selected_letters);
-    console.log("selection_id = " + current_letter);
-    
-    // Determine which letters are covered during the cognitive load phase
-    
-    var equation = cogloadf();
+    // Determine which letter is reveiled during the letter encoding phase
+    if(next_letter === 0) {
+      boxes[0] = "red";
+    }
+    if(next_letter == 1) {
+      boxes[1] = "red";
+    }
+    if(next_letter == 2) {
+      boxes[2] = "red";
+    }
+    if(next_letter == 3) {
+      boxes[3] = "red";
+    }
+    if(next_letter == 4) {
+      boxes[4] = "red";
+    }
     
     html = "<style>";
     html += ".grid-container {";
@@ -105,30 +141,24 @@ var load_display = function(){
   
     html += "<div class='grid-container' style = 'height:400px';>";
     html += "<div class='grid-item'>";
-    html += "<div style = 'display: inline-grid; width: 800px; height: 90px; font-size: 70px; grid: 70px / auto auto auto auto auto auto auto;'>";
-    html += "<div style = 'margin: auto; border: solid black; background: black; width: 90px; height: 90px; padding: 0px 0px;'>";
-    html += "<div style = 'top: 50%;left: 50%;width: 80px; height: 80px; border: solid red;'><span></span></div>";
+    html += "<div style = 'display: inline-grid; width: 800px; height: 90px; font-size: 70px; grid: 70px / auto auto auto auto auto;'>";
+    html += "<div style = 'margin: auto; background:" + boxes[0] + "; width: 90px; height: 90px; padding: 0px 0px;'>";
+    html += "<div style = 'margin: auto;'><span></span></div>";
     html += "</div>";
-    html += "<div style = 'margin: auto; border: solid black; background: black; width: 80px; height: 80px; padding: 0px 0px;'>";
-    html += "<div style = 'margin: auto; padding: 0px 0px; transform: rotate(0deg);'><span></span></div>";
+    html += "<div style = 'margin: auto; background:" + boxes[1] + "; width: 90px; height: 90px; padding: 0px 0px;'>";
+    html += "<div style = 'margin: auto;'><span></span></div>";
     html += "</div>";
-    html += "<div style = 'margin: auto; border: solid black; background: black; width: 80px; height: 80px; padding: 0px 0px;'>";
-    html += "<div style = 'margin: auto; padding: 0px 0px; transform: rotate(0deg);'><span></span></div>";
+    html += "<div style = 'margin: auto; background:" + boxes[2] + "; width: 90px; height: 90px; padding: 0px 0px;'>";
+    html += "<div style = 'margin: auto;'><span></span></div>";
     html += "</div>";
-    html += "<div style = 'margin: auto; border: solid black; background: black; width: 80px; height: 80px; padding: 0px 0px;'>";
-    html += "<div style = 'margin: auto; padding: 0px 0px; transform: rotate(0deg);'><span></span></div>";
+    html += "<div style = 'margin: auto; background:" + boxes[3] + "; width: 90px; height: 90px; padding: 0px 0px;'>";
+    html += "<div style = 'margin: auto;'><span></span></div>";
     html += "</div>";
-    html += "<div style = 'margin: auto; border: solid black; background: black; width: 80px; height: 80px; padding: 0px 0px;'>";
-    html += "<div style = 'margin: auto; padding: 0px 0px; transform: rotate(0deg);'><span></span></div>";
-    html += "</div>";
-    html += "<div style = 'margin: auto;border: solid black; background: black;  width: 80px; height: 80px; padding: 0px 0px;'>";
-    html += "<div style = 'margin: auto; padding: 0px 0px; transform: rotate(0deg);'><span></span></div>";
-    html += "</div>";
-    html += "<div style = 'margin: auto;border: solid black; background: black;  width: 80px; height: 80px; padding: 0px 0px;'>";
-    html += "<div style = 'margin: auto; padding: 0px 0px; transform: rotate(0deg);'><span></span></div>";
+    html += "<div style = 'margin: auto; background:" + boxes[4] + "; width: 90px; height: 90px; padding: 0px 0px;'>";
+    html += "<div style = 'margin: auto;'><span></span></div>";
     html += "</div>";
     html += "</div></div>";
-    html += "<div class='grid-item' style = 'font-size: 70px;'><br><br><br><br><br><br>" + equation + "</div>";
+    html += "<div class='grid-item' style = 'font-size: 70px; color: black'><br><br><br><br><br><br><br><br><br><br></div>";
   
     return html;
-}
+};
--- materials/ospan_adapted/1_setup.js
+++ materials/ospan_adapted/1_setup.js
@@ -3,23 +3,8 @@
     minSetSize = 4; // starting length of each trial (i.e., min number of letters in a trial)
     maxSetSize = 7; // ending length of each trial (i.e., max number of letters in a trial)
     repSet = 3; // number of times each set size should be repeated
-    randomize = true; // present different set sizes in random order. if false, set sizes will be presented in ascending order
-    file_name = null; // file name for data file. if null, a default name consisting of the participant ID and a unique number is chosen.
-    local = true; 
 
 var possibleLetters = ["F","H","J","K","L","N","P","Q","R","S","T","V"];
-
-var setSizes = [];    // different set sizes
-for (var i = minSetSize; i<= maxSetSize; i++){
-  for (var r = 1; r<= repSet; r++){
-    setSizes.push(i);
-  }
-}
-
-var nTrials = setSizes.length; // number of trials
-
-if (randomize){
-  setSizes = jsPsych.randomization.sampleWithoutReplacement(setSizes, nTrials)} // shuffle through the set sizes
 
 var letterMemDemoArray = [3,4,5];  // set sizes of initial demo trials
 var fullDemoArray = [3,4];       // set sizes of full demo trials
@@ -32,42 +17,9 @@ var totalTrials = setSizes.length; //total number of trials in the entire task (
 
 var n = 0; //keeps track of number of trials gone by
 var selection = jsPsych.randomization.sampleWithoutReplacement(possibleLetters, setSizes[n]);
-console.log("the actual selection is " + selection);
 var selection_id = 0; //keeps track of recall items within a test stack
 
 var nLetterRecalled = 0; // feedback
 var nMathAcc = 0; // feedback
 
-var cogloadf = function(){  // generates math questions
-  var possibleOperations = [" + ", " - "];
-  operation = jsPsych.randomization.sampleWithReplacement(possibleOperations, 1)[0];
-  if (operation==" + "){
-    num1 = Math.floor(jStat.uniform.sample(1, 11));
-    num2 =  Math.floor(jStat.uniform.sample(1, 11));
-    ans = num1 + num2;
-  } else if (operation==" - "){
-    num1 = Math.floor(jStat.uniform.sample(1, 11));
-    num2 = Math.floor(jStat.uniform.sample(1, num1));
-    ans = num1 - num2;
-  }
-  
-  var correct = jsPsych.randomization.sampleWithReplacement([true, false], 1)[0];
-  
-  if (!correct){   // generates incorrect answers
-    ansDiff = jsPsych.randomization.sampleWithReplacement([1,2],1)[0];
-    coinFlip = jsPsych.randomization.sampleWithReplacement([true, false],1)[0];
-    if (coinFlip){
-      ans += ansDiff;
-    } else {
-      ans -= ansDiff;
-    }
-    if (ans<0){
-      ans += 2*ansDiff; //ensuring no negative incorrect answers
-    }
-  }
-  
-  var equation = num1+operation+num2+' = '+ans+'<br><br><br><br>';
-  
-  return equation;
-};
 
